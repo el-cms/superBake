edc2a14e72e3ed35674fb8164653dba64656d6a7
Manuel Tancoigne <m.tancoigne@gmail.com>
2013-08-28 05:43:15 +0000

    Update clearCache.sh

--- clearCache.sh
+++ clearCache.sh
@@ -2,16 +2,16 @@
 cache=app/tmp/cache/
 logs=app/tmp/logs
 
-clear
-
-echo "deleting chache files"
 sudo find $cache -type f -print0 | xargs -r -0 rm
+echo "Cache files removed"
 
-echo "resetting logs"
 sudo rm $logs/error.log
 sudo rm $logs/debug.log
+echo "Logs removed"
 
 sudo touch $logs/error.log
 sudo touch $logs/debug.log
+echo "empty logs created"
 
 sudo chown www-data $logs/*.log
+echo "Permissions on logs given to apache webserver"
